01e699174b1f8a0cf9ea09c28345b4817e6d99c4
Denis Burdin <dionisiy.god.of.wine@gmail.com>
2024-10-18 13:50:17 +0300

    reserve

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
+$(document).ready(function () {
+    $("#phone").inputmask({"mask": "+38 (999) 999-99-99"});
+});
 const regModal = document.getElementById('regModal');
 const openReg = document.getElementById('openReg');
 const closeReg = document.getElementById('closeReg');
+const regForm = regModal.lastElementChild
+
 
 const signInModal = document.getElementById('signInModal')
 const openSignIn = document.getElementById('openSignIn')
 const closeSignIn = document.getElementById('closeSignIn')
+const signInForm = signInModal.lastElementChild
 
+const hideForm = (modal, form) => {
+  modal.classList.remove('open');
+  form.reset()
+}
 
-const addEvtListeners = (modal, openBtn, closeBtn) => {
+const addEvtListeners = (modal, openBtn, closeBtn, form) => {
 openBtn.addEventListener('click', ()=> {
     modal.classList.add('open');
   });
   closeBtn.addEventListener('click', ()=> {
-    modal.classList.remove('open');
+    hideForm(modal,form)
   });
   modal.addEventListener('click', (evt)=> {
     if (evt.target === modal) {
-      modal.classList.remove('open');
+      hideForm(modal,form)
     }
   });
   document.addEventListener('keydown', (evt)=> {
     if (evt.key === 'Escape') {
-      modal.classList.remove('open');
+      hideForm(modal,form)
     }
   });
 }
 
-addEvtListeners(regModal, openReg, closeReg)
-addEvtListeners(signInModal, openSignIn, closeSignIn)
-
-
-
-
-console.log('hello world');
-
+addEvtListeners(regModal, openReg, closeReg, regForm)
+addEvtListeners(signInModal, openSignIn, closeSignIn,signInForm)
